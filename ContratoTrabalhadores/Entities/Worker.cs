8f2b8f4c90b9c8614d79ec74854fce74c31a59ca
EZ2DANTAS <matheusdantas2016i9@gmail.com>
2024-01-17 22:38:58 -0300

    Add project files.

---
+++ ContratoTrabalhadores/Entities/Worker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContratoTrabalhadores.Entities.Enums;
+
+namespace ContratoTrabalhadores.Entities
+{
+    internal class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+
+        public Departament Departament { get; set; }
+        List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+        public Worker(string name, WorkerLevel level, double baseSalary,Departament departament)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Departament = departament;
+        }
+
+        public void AddContract( HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract( HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+
+            foreach(var item in Contracts)
+            {
+                if(item.Date.Month == month && item.Date.Year == year)
+                {
+                     sum += item.TotalValue();
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ ContratoTrabalhadores/Program.cs
@@ -0,0 +1,57 @@
+﻿using ContratoTrabalhadores.Entities;
+using ContratoTrabalhadores.Entities.Enums;
+using System;
+using System.Globalization;
+namespace ContratoTrabalhadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter Dapartament`s name: ");
+            string departament = Console.ReadLine();
+
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Level (Junior/MidLevel/Senior): ");
+            WorkerLevel workerLevel = Enum.Parse<WorkerLevel>(Console.ReadLine());
+
+            Console.Write("Base Salary: ");
+            double baseSalary = double.Parse(Console.ReadLine());
+
+            Departament dept = new Departament(departament);
+            Worker worker = new Worker(name, workerLevel, baseSalary, dept);
+
+
+            Console.WriteLine("How many contracts to this worker ?");
+            int quantContracts = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < quantContracts; i++)
+            {
+                Console.WriteLine($"Enter #{i} contract data: ");
+                Console.Write("Date (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.Write("Value Per Hour");
+                double valuePerHour = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                Console.WriteLine("Duration: ");
+                int duration = int.Parse(Console.ReadLine());
+
+                HourContract cont = new HourContract(date, valuePerHour, duration);
+                worker.AddContract(cont);
+            }
+
+            Console.WriteLine("Enter Month and Year to calculate income (MM/YYYY): ");
+            string monthAndYear = Console.ReadLine();
+            int month = int.Parse(monthAndYear.Substring(0, 2));
+            int year = int.Parse(monthAndYear.Substring(3));
+            double valueIncome = worker.Income(year, month);
+
+            Console.WriteLine("\n\n\n");
+            Console.WriteLine("Name: "+worker.Name);
+            Console.WriteLine("Departament: " + worker.Departament.Name) ;
+            Console.WriteLine("Income: for "+monthAndYear+", "+valueIncome);
+        }
+    }
+}
